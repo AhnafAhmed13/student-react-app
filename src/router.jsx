ee0cecaa86a38a917d42988c432e401d47fdca62
Ahnaf Ahmed <61428434+AhnafAhmed13@users.noreply.github.com>
2024-11-06 19:34:55 -0500

    Push Code

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import StudentListPage from './App';
 import { StudentDetailPage } from './pages/StudentDetailPage';
+import { SubmitStudentPage } from './SubmitStudentPage';
 
 // add SubmitStudentPage to the router at the path '/student/submit'
 export const router = createBrowserRouter([
   { path: '/', element: <StudentListPage /> },
   { path: '/student/:id', element: <StudentDetailPage /> },
+  { path: '/student/submit', element: <SubmitStudentPage /> }
 ]);
